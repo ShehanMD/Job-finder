49aed92035092549a6ab34836ac8910c2f701f5e
Menura Abhisheka <mpc9642@gmail.com>
2026-09-25 17:47:24 +0530

    final customBase

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,33 @@ import About from "./pages/About";
 import Home from './pages/Home';
 import EmployerDashboard from './components/Employer/EmployerDashboard';
 import UserProfile from './components/Employer/UserProfile'
+import {customBase} from "./components/firebase/customBase"
+
+
+
+
+customBase.autoInit();  // dont remowe this
+
+
+
+
+  (async () => {
+
+
+    const data2 = await customBase.db.push('ADM', { name: "hellow" })   
+    console.log("put : ", data2);                                               /// example for data add
+
+    const data = await customBase.db.get("ADM")
+    console.log("retrew : ", data);                                         /// example for data read
+
+
+
+
+  })();
+
+
+
+
 
 export default function App() {
   return (
